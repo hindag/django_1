7b63e52eaf37caba28984fa79c4667b98b5db8ac
hind <agtay.hind@gmail.com>
2020-03-29 18:31:34 +0100

    adding tests

--- polls/views.py
+++ polls/views.py
@@ -1,17 +1,29 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Question
-def index(request):
-	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	output = ', '.join([q.question_text for q in latest_question_list])
-	return HttpResponse(output)
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id) 	
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone 
+from .models import Choice, Question
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class DetailView(generic.DetailView):
+	def get_queryset(self):
+		return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+   
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
-# Create your views here.
-
+    ... # same as above, no changes needed.
